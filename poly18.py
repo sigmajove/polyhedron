96317b40bc0cdfb8eaa29f6e681382d1556d49e5
sigmajove <sigmajove@gmail.com>
2025-03-26 23:58:02 -0700

    typo in comment

--- poly18.py
+++ poly18.py
@@ -233,7 +233,7 @@ def make_polygon(half_spaces, size):
     # The preceding algorithm is not so good. On the good side, it
     # is very general, making few assumptions about the input data.
     # On the bad side, it is using floating point calculations to
-    # detect the fact that face faces meet as a single point at the
+    # detect the fact that five faces meet as a single point at the
     # north and south poles. That calcuation is very sensitive to
     # the values ABS_TOL and REL_TOL as the size of the model scales.
 
